93000aefc4653bcaec8dcb0350cc476caf8fa09f
Jackey Yang <jyang@castlepointanime.com>
2022-11-07 14:27:35 -0500

    242 - valid anagram

---
+++ easy/validAnagrams.js
@@ -0,0 +1,23 @@
+// 10-26
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length != t.length) {
+    return false;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    // if (s.length == 0) return true;
+    if (s.indexOf(t[i]) != -1) {
+      s = s.replace(t[i], "");
+      // console.log(s, s.length);
+      if (s.length == 0 && i == t.length - 1) return true;
+    } else {
+      return false;
+    }
+  }
+};
